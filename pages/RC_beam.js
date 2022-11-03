bf70db4d7f9a0562455633adab5edcd0d763543d
kompoj <yord22648@gmail.com>
2022-11-03 17:43:50 +0700

    timestamp 17:43 Thu 03.Nov

--- pages/RC_beam.js
+++ pages/RC_beam.js
@@ -7,8 +7,11 @@ import Nav from "/components/Nav.js"
 import Sidebar from '../components/Sidebar.js'
 import Footer from "../components/Footer.js"
 
+// const scriptpath = require("/a/RCbeamxu.js")
+
 
 export default function RC_beam() {
+
 	return (
 		<>
 			<Head>
@@ -23,10 +26,14 @@ export default function RC_beam() {
 					href="https://fonts.googleapis.com/css2?family=Caveat&family=Open+Sans:wght@100;200;300;400;500;600;700&family=Sarabun:wght@100;200;300;400;500;600;700&display=swap"
 					rel="stylesheet" />
 
+				{/* <script src="https://cdn.plot.ly/plotly-2.12.1.min.js"></script>
+				<script defer src={scriptpath}></script> */}
+
 			</Head>
 			<Script src="https://cdn.plot.ly/plotly-2.12.1.min.js" strategy="beforeInteractive"></Script>
 			<Script type="text/javascript" src="/RCbeamxx.js" strategy="afterInteractive"></Script>
-
+			{/* <Script src="https://cdn.plot.ly/plotly-2.12.1.min.js" strategy="beforeInteractive"></Script>
+			<Script type="text/javascript" src={scriptpath} strategy="afterInteractive"></Script> */}
 
 
 
