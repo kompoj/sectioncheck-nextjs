97ae0340cad4bfa3caad808b9b2df646c78e79b9
kompoj <yord22648@gmail.com>
2022-11-07 17:53:21 +0700

    code prism js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../pageStyles/globals.css'
 // import '../pageStyles/base.css'
 import '../pageStyles/my-theme typora.css'
+import '../pageStyles/prism.css'
 
 import Script from 'next/script'
 
@@ -8,6 +9,7 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
+			<Script type="text/javascript" src="/prism.js" strategy="afterInteractive"></Script>
 			<Component {...pageProps} />
 		</>
 	)
---
+++ pages/code.js
@@ -0,0 +1,48 @@
+﻿export default function code() {
+	return (
+		<>
+
+			<pre style={{ width: 1000, margin: "100px auto" }}>
+				<code className="language-javascript" lang="javascript">
+					import styles from '/componentStyles/Nav.module.css'{"\n"}import Link
+					from 'next/link'{"\n"}
+					{"\n"}import SidebarStyles from '/componentStyles/Sidebar.module.css'{"\n"}
+					{"\n"}const Nav = () =&gt; {"{"}
+					{"\n"}
+					{"\n"}
+					{"\t"}return ({"\n"}
+					{"\t"}
+					{"\t"}&lt;nav className={"{"}styles.nav{"}"}&gt;{"\n"}
+					{"\t"}
+					{"\t"}
+					{"\t"}&lt;svg className={"{"}styles.hamburger{"}"}{" "}
+					xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+					strokeWidth="1.5" stroke="currentColor"&gt;{"\n"}
+					{"\t"}
+					{"\t"}
+					{"\t"}
+					{"\t"}&lt;path strokeLinecap="round" strokeLinejoin="round" d="M3.75
+					6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" /&gt;{"\n"}
+					{"\t"}
+					{"\t"}
+					{"\t"}&lt;/svg&gt;{"\n"}
+					{"\t"}
+					{"\t"}
+					{"\t"}&lt;Link id={"{"}styles.sectioncheck_logo{"}"} href="/"
+					&gt;SectionCheck&lt;/Link&gt;{"\n"}
+					{"\t"}
+					{"\t"}
+					{"\t"}&lt;Link href="/about"&gt;about&lt;/Link&gt;{"\n"}
+					{"\t"}
+					{"\t"}&lt;/nav &gt;{"\n"}
+					{"\t"}){"\n"}
+					{"}"}
+					{"\n"}
+					{"\n"}export default Nav{"\n"}
+				</code>
+			</pre>
+
+
+		</>
+	)
+}
